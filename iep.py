acb34273afae286a1244e3ef3da987308b836c63
Florin OPREA <florin.oprea@alten.com>
2023-02-20 10:55:17 +0200

    ok

--- iep.py
+++ iep.py
@@ -3,7 +3,7 @@ import numpy
 import math
 import imutils
 import numpy as np
-def PolyArray(X, Y, dreapta_lim, stanga_lim):
+def poly_array(X, Y, dreapta_lim, stanga_lim):
         """This function converts a poly array into an opencv contour"""
         h = 470
         c = np.zeros((len(X)+2, 2), np.int32)
